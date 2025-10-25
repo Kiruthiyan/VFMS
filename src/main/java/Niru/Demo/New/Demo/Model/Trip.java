3fade53f42c9f126fac10b8fcf81df05d0cc8d3e
Niru <niruthigan017@gmail.com>
2025-10-25 23:28:00 +0530

    Add files via upload

---
+++ src/main/java/Niru/Demo/New/Demo/Model/Trip.java
@@ -0,0 +1,17 @@
+package Niru.Demo.New.Demo.model;
+
+public class Trip {
+    private Long id;
+    private String destination;
+    private String date;
+
+    public Trip(Long id, String destination, String date) {
+        this.id = id;
+        this.destination = destination;
+        this.date = date;
+    }
+
+    public Long getId() { return id; }
+    public String getDestination() { return destination; }
+    public String getDate() { return date; }
+}
