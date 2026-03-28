98ad601f0958ac9d5d1cfba18537090522cff925
kavishanthn16 <kavishanthn16@gmail.com>
2026-03-28 18:33:58 +0530

    Updated files and added new changes

--- backend/src/main/java/com/vfms/VfmsApplication.java
+++ backend/src/main/java/com/vfms/VfmsApplication.java
@@ -2,8 +2,10 @@ package com.vfms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class VfmsApplication {
 
     public static void main(String[] args) {
