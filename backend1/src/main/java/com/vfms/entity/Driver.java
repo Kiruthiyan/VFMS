3a709dc9ae9e4a1fe5a30878e3fc9ac89c5be62d
hephzibah-peries <hhephzibah2@gmail.com>
2026-04-29 14:17:03 +0530

    env file added and made changes in dashboard

--- backend1/src/main/java/com/vfms/entity/Driver.java
+++ backend1/src/main/java/com/vfms/entity/Driver.java
@@ -18,16 +18,33 @@ public class Driver {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(columnDefinition = "uuid")
     private java.util.UUID id;
 
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "license_number")
     private String licenseNumber;
+
+    @Column(name = "department")
     private String department;
+
+    @Column(name = "joining_date")
     private LocalDate joiningDate;
-    
-    private String licenseStatus; // VALID, EXPIRED, PENDING
-    private Double rating; // 0.0 to 5.0
+
+    @Column(name = "license_status")
+    private String licenseStatus;
+
+    @Column(name = "rating")
+    private Double rating;
+
+    @Column(name = "total_trips")
     private Integer totalTrips;
+
+    @Column(name = "total_distance")
     private Double totalDistance;
+
+    @Column(name = "infractions")
     private Integer infractions;
 }
