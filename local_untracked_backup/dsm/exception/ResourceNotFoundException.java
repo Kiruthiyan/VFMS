1bcd3a40e04e901c97d831573e7c68cab4644deb
Niruthigan P <niruthigan017@gmail.com>
2026-06-14 11:00:03 +0530

    clean: remove obsolete local_untracked_backup folder

--- local_untracked_backup/dsm/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.vfms.dsm.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
